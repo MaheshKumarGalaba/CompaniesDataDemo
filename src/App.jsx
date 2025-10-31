87a831461082b319be54e5184c16f896f1871aca
mahesh <Mahesh.galaba@gpceast.com>
2025-10-31 09:45:40 +0530

    completed all data

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import CentralHub from './components/Centralhub/Centralhub'
+import CentralHub from './components/Centralhub/CentralHub'
 import HomePage from './components/HomePage/HomePage'
 import { FilterProvider } from './components/ContextData/FilterContext'
 
--- src/components/HomePage/HeaderContent.jsx
+++ src/components/HomePage/HeaderContent.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
-import '../../App.css'
-import '../../Index.css'
+
 
 const HeaderContent = () => {
 
